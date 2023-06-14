61e3f20347367f6df49901367644ee55fef75d4c
dgluski <72198073+dgluski@users.noreply.github.com>
2023-06-14 11:54:15 +0200

    Update views.py

--- django_boniek/django_boniek/views.py
+++ django_boniek/django_boniek/views.py
@@ -19,7 +19,7 @@ def mmc_result(request) :
     dico_4_template = {}
     dico_4_template["fighter_blue"] = return_all_fighters(blue_surname,blue_name)
     dico_4_template["fighter_red"]  = return_all_fighters(red_surname,red_name)
-    diff_combats = abs(dico_4_template["fighter_blue"]["nbr_combats"] - dico_4_template["fighter_red"]["nbr_combats"])
+    diff_combats = abs(dico_4_template["fighter_blue"]["nbr_combats_pro"] - dico_4_template["fighter_red"]["nbr_combats_pro"])
     voting_categories = ["Heavyweight","Light Heavyweight","Featherweight","Bantamweight","Flyweight","Strawweight"]
     date_to_time = datetime.strptime(fight_date_form, "%Y-%m-%d")
     if date_to_time < dico_4_template["fighter_blue"]["date_for_next_fight"] or date_to_time < dico_4_template["fighter_red"]["date_for_next_fight"]:
